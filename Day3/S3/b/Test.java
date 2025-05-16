ad3da532bb2ebcc5b262351c13db5b87edd13a7e
jeyarajahananthia <anajeya26@gmail.com>
2025-05-16 23:40:40 +0530

    Add files via upload

---
+++ Day3/S3/b/Test.java
@@ -0,0 +1,14 @@
+package b;
+
+public class Test{
+	 void print(){
+		System.out.println("Hi from class Test in B");
+	}
+	
+	public void display(){
+		print();
+		System.out.println("Display : Hi from class Test in B");
+	}		
+		
+		
+}
